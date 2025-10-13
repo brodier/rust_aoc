40d50e7b3cd335624fff4c8bfb416bc91561c0eb
Bernard Rodier <bernard.rodier@gmail.com>
2025-10-13 14:29:57 +0200

    Some refactoring on day04 to improve perf. But not very effective

--- src/year2023/day04.rs
+++ src/year2023/day04.rs
@@ -1,12 +1,10 @@
 use crate::utils::common::parse_usize;
 
-const MAX_CARD: usize = 201;
+const MAX_CARD_VAL: usize = 100;
 
 #[derive(Debug)]
 struct Puzzle {
-   cards:Vec<Card>,
-   result1:usize,
-   result2:usize,
+   scores:Vec<usize>
 }
 
 #[derive(Debug)]
@@ -15,67 +13,44 @@ struct Card {
     draw_numbers:Vec<usize>,
 }
 
+impl Card {
+    fn eval(&self) -> usize {
+        let mut founds = [false; MAX_CARD_VAL];
+        self.winning_numbers.iter().for_each(|&card| founds[card] = true );
+        self.draw_numbers.iter().filter(|&&card| founds[card]).count()
+    }
+}
+
 impl Puzzle {
     fn build(lines: &str) -> Puzzle {
         let mut cards:Vec<Card> = Vec::new();
         for line in lines.lines() {
             let (_,rest) = line.split_once(":").unwrap();
             let (winning,draw) = rest.split_once("|").unwrap();
-            let mut winning_numbers = parse_usize(winning);
-            winning_numbers.sort();
-            let mut draw_numbers = parse_usize(draw);
-            draw_numbers.sort();
-            let card = Card{winning_numbers,draw_numbers};
+            let card = Card{winning_numbers: parse_usize(winning),draw_numbers: parse_usize(draw)};
             cards.push(card);
         }
-        Puzzle{cards,result1:0,result2:0}
+        Puzzle{scores: cards.iter().map(|c| c.eval()).collect()}
     }
 
-    fn eval(&mut self) {
-        let mut score = 0;
-        let mut nb_copies:[usize;MAX_CARD] = [0; MAX_CARD];
+    fn result1(&self) -> usize {
+        self.scores.iter().map(|s| (1 << s) >> 1 ).sum()
+    }
 
-        for i in 0..self.cards.len() {
-            let nb_cards_i = nb_copies[i] + 1;
-            let card = &self.cards[i];
-            let mut w = 0;
-            let mut d = 0;
-            let mut card_score:u32 = 0;
-            while w < card.winning_numbers.len() && d < card.draw_numbers.len() {
-                if card.winning_numbers[w] == card.draw_numbers[d] {
-                    w += 1;
-                    d += 1;
-                    card_score += 1;
-                } else if card.winning_numbers[w] < card.draw_numbers[d] {
-                    w += 1;
-                } else {
-                    d += 1;
-                }
-            }
-            if card_score > 0 {
-                let from = i+1;
-                let to = from+card_score as usize;
-                for j in from..to {
-                    nb_copies[j] += nb_cards_i;
-                }
-            }
-            // eprintln!("Card {:?} score {}", card, card_score);
-            if card_score > 0{
-                score += 2_usize.pow(card_score - 1);
-            }
-            
+    fn result2(&self) -> usize {
+        let mut copies = vec![1; self.scores.len()];
+        for (i, &s) in self.scores.iter().enumerate() {
+            (1..=s).for_each(|j| copies[i + j] += copies[i]);
         }
-        self.result1 = score;
-        self.result2 = nb_copies.iter().sum::<usize>() + self.cards.len();
+        copies.iter().sum()
     }
 }
 
 pub fn solve(step: usize, input: String) -> String {
-    let mut puzzle = Puzzle::build(&input);
-    puzzle.eval();
+    let puzzle = Puzzle::build(&input);
     if step == 1 {
-        puzzle.result1.to_string()
+        puzzle.result1().to_string()
     } else {
-        puzzle.result2.to_string()
+        puzzle.result2().to_string()
     }
 }
