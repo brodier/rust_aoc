58d0ce21ac45dcfee0ecc372d4f2356de2bfc3b8
Bernard Rodier <bernard.rodier@gmail.com>
2025-10-15 11:46:11 +0200

    Completing day10 part 2

--- src/year2023/day10.rs
+++ src/year2023/day10.rs
@@ -78,10 +78,10 @@ impl Animal {
 
     fn build(grid:&Grid) -> Animal {
         let i = grid.input.find("S").unwrap();
-        eprintln!("S is at {}", i);
+        // println!("S is at {}", i);
         let init_pos = (i%(grid.size.0+1),i/(grid.size.0+1));
         let mut animal = Animal{init_pos, pos:init_pos.clone(), orient:Orient::UP};
-        eprintln!("Start from {:?} Grid size {:?}", animal.init_pos, grid.size);
+        // println!("Start from {:?} Grid size {:?}", animal.init_pos, grid.size);
         animal.init_orient(grid);
         animal 
     }
@@ -94,7 +94,6 @@ impl Animal {
                 return;
             }
             self.pos = self.init_pos;
-            println!("Orient {:?} not working", self.orient);
         }
         panic!("Not any valid start orientation");   
     }
@@ -113,13 +112,14 @@ impl Animal {
         self.pos = new_pos.unwrap();
         let new_orient = self.update_orient(grid);
         if new_orient.is_err() {
-            eprintln!("New pos {:?} not compatible with orient", self.pos);
+            if grid.get(self.pos) != b'S' {
+                eprintln!("New pos {:?} not compatible with orient", self.pos);
+            }
         }  
         return new_orient;
     }
 
     fn update_orient(&mut self, grid:&Grid) -> Result<Orient,GridError> {
-        eprintln!("{:?}", self.pos);
         match self.orient {
             Orient::UP => {
                 match grid.get(self.pos) {
@@ -166,16 +166,43 @@ impl Puzzle<'_> {
         let grid = Grid::build(self.input);
         let mut animal = Animal::build(&grid);
         let mut counter = 1;
-        eprintln!("ready to walk with animal: {:?}", animal);
+        // println!("ready to walk with animal: {:?}", animal);
+        let mut points = Vec::new();
+        points.push(animal.init_pos);
         while let Ok(new_orient) = animal.walk(&grid) {
+            match grid.get(animal.pos) {
+                b'7' | b'J' | b'F' | b'L' => points.push(animal.pos),
+                _ => {}
+            }
             animal.orient = new_orient;
             counter += 1;
         }
-        (counter/2).to_string()
+        counter /= 2;
+        // println!("Computed counter : {}", counter);
+        if self.step == 1 {
+            counter.to_string()
+        } else {
+            (self.get_nb_interior_tiles(points) + 1 - counter).to_string()
+        }
+    }
+
+    fn get_nb_interior_tiles(&self, points:Vec<(usize,usize)>) -> usize {
+        let a = points.last().unwrap();
+        let mut a = (a.0 as i64, a.1 as i64);
+        let mut sum:i64 = 0;
+        for itt in points.iter() {
+            let b:(i64,i64) = (itt.0 as i64, itt.1 as i64);
+            sum += a.1 * b.0 - a.0 * b.1;
+            a = b;
+        }
+        sum /= 2;
+        // println!("Computed area : {}", sum);
+        sum.abs() as usize
     }
 }
 
 pub fn solve(step: usize, input: String) -> String {
     let p = Puzzle::build(step, &input);
+    // println!("Puzzle:\n{}", p.input);
     p.solve()
 }
--- tests/year2023/day10.rs
+++ tests/year2023/day10.rs
@@ -14,6 +14,40 @@ SJ.L7
 |F--J
 LJ...";
 
+const EXAMPLE3: &str = "\
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........";
+
+const EXAMPLE4: &str = "\
+..........
+.S------7.
+.|F----7|.
+.||....||.
+.||....||.
+.|L-7F-J|.
+.|..||..|.
+.L--JL--J.
+..........";
+
+const EXAMPLE5: &str = "\
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ...";
+
 
 #[test]
 fn part1_test() {
@@ -23,5 +57,9 @@ fn part1_test() {
 
 #[test]
 fn part2_test() {
-    assert_eq!(solve(2,EXAMPLE.to_string()), "4");
+    assert_eq!(solve(2,EXAMPLE.to_string()), "1");
+    assert_eq!(solve(2,EXAMPLE2.to_string()), "1");
+    assert_eq!(solve(2,EXAMPLE3.to_string()), "4");
+    assert_eq!(solve(2,EXAMPLE4.to_string()), "4");
+    assert_eq!(solve(2,EXAMPLE5.to_string()), "8");
 }
