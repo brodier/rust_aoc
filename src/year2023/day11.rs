03387f08736b0a75dfa763787206022363cef0c9
Bernard Rodier <bernard.rodier@gmail.com>
2025-10-15 13:13:51 +0200

    Completing day11 part 1

--- src/year2023/day11.rs
+++ src/year2023/day11.rs
@@ -1,20 +1,70 @@
 #[derive(Debug)]
 struct Puzzle<'a> {
-    step:usize, 
+    _step:usize, 
     input: &'a str,
 }
 
+#[derive(Debug)]
+struct Univers {
+    galaxies:Vec<(usize,usize)>
+}
+
+impl Univers {
+    fn build(input:&str) -> Univers {
+        let mut galaxies = Vec::new();
+        let mut line_num = 0;
+        // 1. look for expended line
+        for line in input.lines() {
+            if !line.contains("#") {
+                // expend this line of the univers
+                line_num += 1;
+            } else {
+                for (x,b) in line.bytes().enumerate() {
+                    if b == b'#' {
+                        galaxies.push((x, line_num));
+                    }
+                }
+            }
+            line_num +=1;
+        }
+        // need to expend univers column by incressing x with some offset if not found galaxy on the current col
+        galaxies.sort();
+        let mut offset = 0;
+        let mut last_x = galaxies.first().unwrap().0;
+        for g in galaxies.iter_mut() {
+            if last_x != g.0 && last_x + 1 != g.0 {
+                    offset += g.0 - last_x - 1;
+            }
+            last_x = g.0;
+            g.0 += offset;
+        }
+        Univers{galaxies}        
+    }
+
+    fn sum_distance(&self) -> usize {
+        let mut sum = 0;
+        for i in 0..self.galaxies.len() {
+            for j in (i+1)..self.galaxies.len() {
+                let gi = self.galaxies[i];
+                let gj = self.galaxies[j];
+                sum += gi.0.abs_diff(gj.0) + gi.1.abs_diff(gj.1);
+            }
+        }
+        sum
+    }
+}
 impl Puzzle<'_> {
     fn build<'a>(step:usize, input:&'a str) -> Puzzle<'a> {
-        Puzzle { step, input }
+        Puzzle { _step: step, input }
     }
 
     fn solve(&self) -> String {
-        0.to_string()
+        let u = Univers::build(self.input);
+        // println!("Univers : {:?}", u);
+        u.sum_distance().to_string()
     }
 }
 pub fn solve(step: usize, input: String) -> String {
     let p = Puzzle::build(step, &input);
-    // println!("Puzzle:\n{}", p.input);
     p.solve()
 }
--- tests/year2023/day11.rs
+++ tests/year2023/day11.rs
@@ -1,14 +1,23 @@
 use aoc::year2023::day11::*;
 
 const EXAMPLE: &str = "\
-";
+...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....";
 
 #[test]
 fn part1_test() {
-    assert_eq!(solve(1, EXAMPLE.to_string()), "0");
+    assert_eq!(solve(1, EXAMPLE.to_string()), "374");
 }
 
 #[test]
 fn part2_test() {
-    assert_eq!(solve(2,EXAMPLE.to_string()), "0");
+    assert_eq!(solve(2,EXAMPLE.to_string()), "374");
 }
