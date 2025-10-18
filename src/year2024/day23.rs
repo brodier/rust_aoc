4993191003f961a4552c2f67aca5f38d5622c64e
Bernard Rodier <bernard.rodier@gmail.com>
2025-10-18 17:07:53 +0200

    Improve year 2024 day 23 solution

--- src/year2024/day23.rs
+++ src/year2024/day23.rs
@@ -1,78 +1,102 @@
-use std::collections::HashMap;
-
-fn get_id(pc_name:&str) -> usize {
-    let pc = pc_name.as_bytes();
-    (pc[1] as usize)* 256 + pc[0] as usize
-}
-
-
 fn filter(group:&(usize,usize,usize)) -> bool {
-    ((group.0 & 0xff) as u8) == b't' ||
-    ((group.1 & 0xff) as u8) == b't' ||
-    ((group.2 & 0xff) as u8) == b't'
+    ((group.0 / 26) as u8) + b'a' == b't' ||
+    ((group.1 / 26) as u8) + b'a' == b't' ||
+    ((group.2 / 26) as u8) + b'a' == b't'
 }
 
 
 fn get_password(group:&Vec<usize>) -> String {
      let mut result:Vec<String> = group.iter().map(|id| {
-        let name = [(id % 256) as u8, (id >> 8) as u8];
+        let name = [(id / 26) as u8 + b'a', (id % 26) as u8 + b'a'];
         String::from_utf8(name.to_vec()).unwrap()
     }).collect();
      result.sort();
      result.join(",")
 }
 
-fn joining_groups(links_by_pc:&HashMap<usize,Vec<usize>>, new_group:&mut Vec<usize>) {
-    for pc in links_by_pc.keys() {
-        if new_group.iter().all(|m| links_by_pc.get(pc).unwrap().contains(m)) {
-            new_group.push(*pc);
-        }
+struct LinkByPc {
+    links_by_pc:[Option<Vec<usize>>;26*26],
+    all_pc:Vec<usize>,
+}
+
+impl LinkByPc {
+    fn build() -> LinkByPc {
+        LinkByPc { links_by_pc: [const {None};26*26],all_pc:Vec::new()}
     }
-    
+
+    fn index(pc_name:&str) -> usize {
+        let a =pc_name.as_bytes();
+        let index = (a[0] - b'a') as usize * 26 + a[1] as usize - b'a' as usize;
+        index
+    }
+
+    fn init(&mut self, name: &str) -> (usize,bool) {
+        let pc = LinkByPc::index(name);
+        let mut new_pc = false;
+        if self.links_by_pc[pc].is_none() {
+            new_pc = true;
+            self.links_by_pc[pc] = Some(Vec::new());
+            self.all_pc.push(pc);
+        }
+        return (pc, new_pc);
+    }
+
+    fn link(&mut self, name1:&str, name2:&str) -> (bool, usize, usize) {
+        let (pc1, pc1_is_new) = self.init(name1);
+        let (pc2, pc2_is_new) = self.init(name2);
+        self.links_by_pc[pc1].as_mut().unwrap().push(pc2);
+        self.links_by_pc[pc2].as_mut().unwrap().push(pc1);
+        (pc1_is_new||pc2_is_new, pc1, pc2)
+    }
+
+    fn get_common_pcs(&self, pc1:usize, pc2:usize) -> Vec<usize> {
+        let list_connected_to_pc1 = self.links_by_pc[pc1].as_ref().unwrap();
+        let list_connected_to_pc2 = self.links_by_pc[pc2].as_ref().unwrap();
+        list_connected_to_pc2.iter().filter(|&one_pc_connected_to_pc2|
+             list_connected_to_pc1.contains(one_pc_connected_to_pc2)).map(|pc| *pc).collect()
+
+    }
 }
 
 pub fn solve(part:usize, input:String) -> String {
-    let mut links_by_pc:HashMap<usize,Vec<usize>> = HashMap::new();
-    let mut multi_group:Vec<Vec<usize>> = Vec::new();
+    let mut links_by_pc = LinkByPc::build();
+    let mut best_group:Vec<usize> = Vec::new();
+    let mut max_len = 0;
     let mut groups = Vec::new();
     for line in input.lines() {
         let (name1,name2) = line.split_once("-").unwrap();
-        let (pc1,pc2) = (get_id(name1), get_id(name2));
-        let mut new_pc = false;
-        if !links_by_pc.contains_key(&pc1) {
-            new_pc = true;
-            links_by_pc.insert(pc1, Vec::new());
-        }
-        if !links_by_pc.contains_key(&pc2) {
-            new_pc = true;
-            links_by_pc.insert(pc2, Vec::new());
-        }
-        links_by_pc.get_mut(&pc1).unwrap().push(pc2);
-        links_by_pc.get_mut(&pc2).unwrap().push(pc1);
+        let (new_pc, pc1, pc2) = links_by_pc.link(name1, name2);
         if !new_pc {
-            let mut pc3_list = Vec::new();
-            let pc2_list =links_by_pc.get(&pc2).unwrap();
-            let pc1_list = links_by_pc.get(&pc1).unwrap();
-            pc1_list.iter().for_each(|&pc3| 
-                if pc2_list.contains(&pc3) {
-                    pc3_list.push(pc3);
-                }
-            );
+            let pc3_list = links_by_pc.get_common_pcs(pc1, pc2);
             // for step 2 for each pc in pc3_list             
             for pc3 in pc3_list {
                 groups.push((pc1,pc2,pc3));
-
-                let mut new_group = vec![pc1,pc2,pc3];
-                joining_groups(&links_by_pc, &mut new_group);
-                multi_group.push(new_group);
+                if part == 2 {
+                    let mut new_group = vec![pc1,pc2,pc3];
+                    joining_groups(&links_by_pc, &mut new_group);
+                    let new_len = new_group.len();
+                    if new_len > max_len {
+                        best_group = new_group;
+                        max_len = new_len;
+                    }
+                }
             }
         }
     }
     if part == 1 {
         groups.iter().filter(|&g| filter(g)).count().to_string()    
     } else {
-        multi_group.sort_unstable_by(|a,b| b.len().cmp(&a.len()));
-        get_password(multi_group.first().unwrap())
+        get_password(&best_group)
     }
     
 }
+
+fn joining_groups(links_by_pc:&LinkByPc, new_group:&mut Vec<usize>) {
+    for &pc in links_by_pc.all_pc.iter() {
+        let pc_links = links_by_pc.links_by_pc[pc].as_ref().unwrap();
+        if new_group.iter().all(|m| pc_links.contains(m)) {
+            new_group.push(pc);
+        }
+    }
+}
+
