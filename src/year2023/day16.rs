70a54f5a142fe2667dfbc604e55393a5069d07a4
Bernard Rodier <bernard.rodier@gmail.com>
2025-10-25 18:29:35 +0200

    Solving day 16 but it seems very poor solution, optimization needed

--- src/year2023/day16.rs
+++ src/year2023/day16.rs
@@ -1,13 +1,207 @@
-type ParseResult = String;
 
-pub fn parse(input:String) -> ParseResult {
-    input
+#[derive(Clone,Debug)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
 }
 
-pub fn part1(_:&ParseResult) -> String {
-    "1".to_string()
+#[derive(Clone,Debug)]
+pub struct Grid {
+    grid: Vec<u8>,
+    width: usize,
+    height: usize,
 }
 
-pub fn part2(_:&ParseResult) -> String {
-    "2".to_string()
+#[derive(Clone, Debug)]
+struct BeamState {
+    pos:(usize, usize),
+    direction:Direction,
+}
+
+impl BeamState {
+
+    fn id(&self, gird:&Grid) -> usize {
+        match self.direction {
+            Direction::Up => self.pos.1 * gird.width + self.pos.0,
+            Direction::Down => (gird.height + self.pos.1) * gird.width + self.pos.0,
+            Direction::Left => (2 * gird.height + self.pos.1) * gird.width + self.pos.0,
+            Direction::Right => (3 * gird.height + self.pos.1) * gird.width + self.pos.0,
+        }
+    }
+
+    fn next(&self, grid:&Grid) -> (Option<BeamState>, Option<BeamState>) {
+        match self.direction {
+            Direction::Up => self.next_up(grid),
+            Direction::Down => self.next_down(grid),
+            Direction::Left => self.next_left(grid),
+            Direction::Right => self.next_right(grid),
+        }
+    }
+
+    
+    fn init_dir(&self, on:u8) -> (Option<BeamState>, Option<BeamState>) {
+        let mut result = self.clone();
+        match (on, self.direction.clone()) {
+            (b'.', _) => return (Some(result), None),
+            (b'-', Direction::Left|Direction::Right) => { return (Some(result), None); },
+            (b'|', Direction::Down|Direction::Up) => { return (Some(result), None); },
+            (b'/', Direction::Left) => { result.direction = Direction::Down; return (Some(result), None); },
+            (b'/', Direction::Right) => { result.direction = Direction::Up; return (Some(result), None); },
+            (b'/', Direction::Down) => { result.direction = Direction::Left; return (Some(result), None); },
+            (b'/', Direction::Up) => { result.direction = Direction::Right; return (Some(result), None); },
+            (b'\\', Direction::Left) => { result.direction = Direction::Up; return (Some(result), None); },
+            (b'\\', Direction::Right) => { result.direction = Direction::Down; return (Some(result), None); },
+            (b'\\', Direction::Down) => { result.direction = Direction::Right; return (Some(result), None); },
+            (b'\\', Direction::Up) => { result.direction = Direction::Left; return (Some(result), None); },
+            (b'|', Direction::Left|Direction::Right) => { 
+                let mut result2 = result.clone();
+                result.direction = Direction::Up; 
+                result2.direction = Direction::Down; 
+                return (Some(result), Some(result2)); 
+            },
+            (b'-', Direction::Down|Direction::Up) => {
+                let mut result2 = result.clone();
+                result.direction = Direction::Left; 
+                result2.direction = Direction::Right;
+                return (Some(result), Some(result2)); },
+            _ => unreachable!()
+        }
+    }
+
+    fn next_up(&self, grid:&Grid) -> (Option<BeamState>, Option<BeamState>) {
+        if self.pos.1 == 0 {
+            return (None,None);
+        }
+        let new_pos = (self.pos.0, self.pos.1 - 1);
+        let new_pos_is = grid.grid[new_pos.1 * (grid.width + 1) + new_pos.0];
+        match new_pos_is {
+            b'.' => return (Some(BeamState{pos:new_pos, direction:Direction::Up}), None),
+            b'/' => return (Some(BeamState{pos:new_pos, direction:Direction::Right}), None),
+            b'\\' => return (Some(BeamState{pos:new_pos, direction:Direction::Left}), None),
+            b'|' => return (Some(BeamState{pos:new_pos, direction:Direction::Up}), None),
+            b'-' => return (Some(BeamState{pos:new_pos, direction:Direction::Right}), Some(BeamState{pos:new_pos, direction:Direction::Left})),
+            _ => unreachable!()
+        }
+    }
+
+    fn next_down(&self, grid:&Grid) -> (Option<BeamState>, Option<BeamState>) {
+        if self.pos.1 == grid.height - 1 {
+            return (None,None);
+        }
+        let new_pos = (self.pos.0, self.pos.1 + 1);
+        let new_pos_is = grid.grid[new_pos.1 * (grid.width + 1) + new_pos.0];
+        match new_pos_is {
+            b'.' => return (Some(BeamState{pos:new_pos, direction:Direction::Down}), None),
+            b'/' => return (Some(BeamState{pos:new_pos, direction:Direction::Left}), None),
+            b'\\' => return (Some(BeamState{pos:new_pos, direction:Direction::Right}), None),
+            b'|' => return (Some(BeamState{pos:new_pos, direction:Direction::Down}), None),
+            b'-' => return (Some(BeamState{pos:new_pos, direction:Direction::Right}), Some(BeamState{pos:new_pos, direction:Direction::Left})),
+            _ => unreachable!()
+        }
+    }
+
+    fn next_left(&self, grid:&Grid) -> (Option<BeamState>, Option<BeamState>) {
+        if self.pos.0 == 0 {
+            return (None,None);
+        }
+        let new_pos = (self.pos.0 - 1, self.pos.1);
+        let new_pos_is = grid.grid[new_pos.1 * (grid.width + 1) + new_pos.0];
+        match new_pos_is {
+            b'.' => return (Some(BeamState{pos:new_pos, direction:Direction::Left}), None),
+            b'/' => return (Some(BeamState{pos:new_pos, direction:Direction::Down}), None),
+            b'\\' => return (Some(BeamState{pos:new_pos, direction:Direction::Up}), None),
+            b'-' => return (Some(BeamState{pos:new_pos, direction:Direction::Left}), None),
+            b'|' => return (Some(BeamState{pos:new_pos, direction:Direction::Down}), Some(BeamState{pos:new_pos, direction:Direction::Up})),
+            _ => unreachable!()
+        }
+    }
+
+    fn next_right(&self, grid:&Grid) -> (Option<BeamState>, Option<BeamState>) {
+        if self.pos.0 == grid.width - 1 {
+            return (None,None);
+        }
+        let new_pos = (self.pos.0 + 1, self.pos.1);
+        let new_pos_is = grid.grid[new_pos.1 * (grid.width + 1) + new_pos.0];
+        match new_pos_is {
+            b'.' => return (Some(BeamState{pos:new_pos, direction:Direction::Right}), None),
+            b'/' => return (Some(BeamState{pos:new_pos, direction:Direction::Up}), None),
+            b'\\' => return (Some(BeamState{pos:new_pos, direction:Direction::Down}), None),
+            b'-' => return (Some(BeamState{pos:new_pos, direction:Direction::Right}), None),
+            b'|' => return (Some(BeamState{pos:new_pos, direction:Direction::Down}), Some(BeamState{pos:new_pos, direction:Direction::Up})),
+            _ => unreachable!()
+        }
+    }
+   
+}
+
+pub fn parse(input:String) -> Grid {
+    let width = input.lines().next().unwrap().len();
+    let height = input.lines().count();
+    Grid{grid:input.as_bytes().to_vec(), width, height}
+}
+
+fn solve(init_beam:&BeamState, grid:&Grid) -> usize {
+    let mut beams:Vec<BeamState> = Vec::new();
+    let mut loop_breaker = vec![false;grid.width * grid.height * 4];
+    let mut energized_position = vec![false;grid.width * grid.height];
+    let index = init_beam.pos.1 * grid.width + init_beam.pos.0;
+    energized_position[index] = true;
+    let start_on = grid.grid[init_beam.pos.1 * (grid.width + 1) + init_beam.pos.0];
+    let init_beams = init_beam.init_dir(start_on);
+    if let Some(b) = init_beams.0 {
+        loop_breaker[b.id(grid)] = true;
+        beams.push(b);
+    }
+    if let Some(b) = init_beams.1 {
+        loop_breaker[b.id(grid)] = true;
+        beams.push(b);
+    }
+    while beams.is_empty() == false {
+        let beam = beams.pop().unwrap();
+        let (next1, next2) = beam.next(grid);
+        if let Some(n1) = next1 {
+            let index = n1.pos.1 * grid.width + n1.pos.0;
+            energized_position[index] = true;
+            if !loop_breaker[n1.id(grid)] {
+                loop_breaker[n1.id(grid)] = true;
+                beams.push(n1);
+            }
+        }
+        if let Some(n2) = next2 {
+            let index = n2.pos.1 * grid.width + n2.pos.0;
+            energized_position[index] = true;
+            if !loop_breaker[n2.id(grid)] {
+                loop_breaker[n2.id(grid)] = true;
+                beams.push(n2);
+            }
+        }
+    }
+    energized_position.iter().filter(|&&v| v).count()
+}
+
+
+pub fn part1(grid:&Grid) -> String {
+    let init_beam = BeamState{pos:(0,0), direction:Direction::Right};
+    solve(&init_beam, grid).to_string()
+}
+
+pub fn part2(grid:&Grid) -> String {
+    let mut max = 0;
+    for i in 0..grid.width {
+        let init_beam = BeamState{pos:(i,0), direction:Direction::Down};
+        let res1 = solve(&init_beam, grid);
+        let init_beam = BeamState{pos:(i,grid.height-1), direction:Direction::Up};
+        let res2 = solve(&init_beam, grid);
+        max = res1.max(res2).max(max);
+    }
+    for i in 0..grid.height {
+        let init_beam = BeamState{pos:(0,i), direction:Direction::Right};
+        let res1 = solve(&init_beam, grid);
+        let init_beam = BeamState{pos:(grid.width-1,i), direction:Direction::Left};
+        let res2 = solve(&init_beam, grid);
+        max = res1.max(res2).max(max);
+    }
+    max.to_string()
 }
--- tests/year2023/day16.rs
+++ tests/year2023/day16.rs
@@ -1,14 +1,23 @@
 use aoc::year2023::day16::*;
 
 const EXAMPLE: &str = "\
-";
+.|...\\....
+|.-.\\.....
+.....|-...
+........|.
+..........
+.........\
+..../.\\\\..
+.-.-/..|..
+.|....-|.\\
+..//.|....";
 
 #[test]
 fn part1_test() {
-    assert_eq!(part1(&parse(EXAMPLE.to_string())), "1");
+    assert_eq!(part1(&parse(EXAMPLE.to_string())), "46");
 }
 
 #[test]
 fn part2_test() {
-    assert_eq!(part2(&parse(EXAMPLE.to_string())), "2");
+    assert_eq!(part2(&parse(EXAMPLE.to_string())), "51");
 }
