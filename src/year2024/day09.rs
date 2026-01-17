995cd5064d289f7ec5095f46853f58011d8cb3ae
Bernard Rodier <bernard.rodier@gmail.com>
2026-01-17 09:59:26 +0100

    few improvements Replacing BTreeset by BinaryHeap<Reverse<usize>> improving readability by replacing Vec<Vec<i32>> by structure data:Vec<Block>, free:usize

--- src/year2024/day09.rs
+++ src/year2024/day09.rs
@@ -1,15 +1,34 @@
-use std::{collections::BTreeSet, str::{Chars, from_utf8}, usize};
-
-const FREE:i32 = -1;
+use std::{cmp::Reverse, collections::BinaryHeap, str::{Chars, from_utf8}, usize};
 
 #[derive(Clone, Debug)]
 struct Block {
-    id: i32,
+    id: usize,
     length: usize
 }
 
+#[derive(Clone, Debug)]
+struct Span {
+    data: Vec<Block>,
+    free: usize,
+}
+
+impl Span {
+
+    fn build(id:usize, length:u8) -> Span {
+        Span{data:vec![Block{id, length: length as usize}], free:0}
+    }
+
+    fn with_free(&mut self, free_length:u8) {
+        self.free = free_length as usize;
+    }
+
+}
+const MAX_SPAN_LEN:usize=9;
+const SPAN_NOT_FOUND:(usize,usize) = (MAX_SPAN_LEN,0);
+
+
 struct FreeSpanIndexor {
-    spans_by_length: [BTreeSet<usize>;9]
+    spans_by_length: [BinaryHeap<Reverse<usize>>;MAX_SPAN_LEN]
 }
 
 impl FreeSpanIndexor {
@@ -19,62 +38,57 @@ impl FreeSpanIndexor {
 
     fn push(&mut self, free_idx:usize, free_len:usize) {
         if free_len > 9 {
-            panic!("Unsupported free len upper 9");
+            unreachable!("Unsupported free len upper 9");
         } else if free_len > 0 {
-            self.spans_by_length[free_len-1].insert(free_idx);
+            self.spans_by_length[free_len-1].push(Reverse(free_idx));
         }
     }
 
     fn search_idx(&mut self, block_len:usize, idx_limit:usize) -> Option<usize> {
-        let mut candidates = Vec::new();
+        let mut min = SPAN_NOT_FOUND;
         for i in (block_len-1)..9 {
-            if let  Some(&idx) = self.spans_by_length[i].first() {
-                if  idx < idx_limit {
-                    candidates.push((idx,i));
+            if let  Some(&idx) = self.spans_by_length[i].peek() {
+                if  idx.0 < idx_limit && ( min.0 == 9 || min.1 > idx.0){
+                    min = (i,idx.0);
                 }
             }
         }
-        if candidates.is_empty() {
-            return None;
+        if min == SPAN_NOT_FOUND {
+            return None
+        } else {
+            self.spans_by_length[min.0].pop();
+            if min.0 + 1 > block_len {
+                self.spans_by_length[min.0 - block_len].push(Reverse(min.1));
+            }
+            Some(min.1)
         }
-        candidates.sort();
-        let selected = candidates.first().unwrap();
-        self.spans_by_length[selected.1].pop_first();
-        let remaining_free_len = (selected.1 + 1) - block_len;
-        let free_idx = Some(selected.0);
-        if remaining_free_len > 0 {
-            self.spans_by_length[remaining_free_len-1].insert(selected.0);
-        }
-        free_idx
     }
 }
 
 #[derive(Clone)]
 pub struct FileMap {
-    blocks: Vec<Vec<Block>>
+    blocks: Vec<Span>
 }
 
 impl FileMap {
     pub fn load(chars:Chars<'_>) -> FileMap {
-        let mut read_block = true;
         let mut blocks = Vec::new();
-        let mut curr_block = Block{id:0, length:0};
+        let mut curr_block: Option<Span> = None;
         for c in chars {        
             let s:u8 = c as u8 - b'0';
-            if read_block && s==0 {
+            if curr_block.is_none() && s==0 {
                 panic!("unexpected block size");
             }
-            read_block = if read_block { 
-                curr_block.length = s as usize;
-                false 
-            } else { 
-                blocks.push(vec![curr_block.clone(), Block{id:FREE, length: s as usize}]);
-                curr_block.id += 1;
-                true 
-            };
+            if let Some(mut block) = curr_block {
+                block.with_free(s);
+                blocks.push(block);
+                curr_block = None;
+            } else {
+                curr_block = Some(Span::build(blocks.len(), s))
+            }
         }
-        if read_block == false {
-            blocks.push(vec![curr_block, Block{id:FREE, length: 0}]);
+        if let Some(last_block) = curr_block {
+            blocks.push(last_block);
         }
         FileMap{blocks}
     }
@@ -83,57 +97,50 @@ impl FileMap {
         let mut block_itt = self.blocks.len() - 1;
         let mut free_itt = 0;
         while free_itt < block_itt {
-            let mut remaining_length = self.blocks[block_itt][0].length;
-            let file_id =  self.blocks[block_itt][0].id;
+            let mut remaining_length = self.blocks[block_itt].data[0].length;
+            let file_id =  self.blocks[block_itt].data[0].id;
             loop {
-                let free_len = self.blocks[free_itt].last().unwrap().length;
+                let free_len = self.blocks[free_itt].free;
                 if free_itt < block_itt && free_len < remaining_length {
                     remaining_length -= free_len;
-                    self.blocks[free_itt].last_mut().unwrap().id = file_id;
+                    self.blocks[block_itt-1].free += free_len;
+                    self.blocks[block_itt].data[0].length -= free_len;
+                    self.blocks[free_itt].data.push(Block { id: file_id, length: free_len });
+                    self.blocks[free_itt].free = 0;
                     free_itt+=1;
                 } else if free_itt == block_itt {
-                    let moved_len = self.blocks[block_itt][0].length - remaining_length;
-                    self.blocks[block_itt][0].length=remaining_length;
-                    self.blocks[free_itt].insert(1, Block { id: FREE, length: moved_len });
+                    let moved_len = self.blocks[block_itt].data[0].length - remaining_length;
+                    self.blocks[block_itt].data[0].length=remaining_length;
+                    self.blocks[free_itt].free += moved_len;
                     break;
                 } else { 
-                    let remain_free_len = free_len - remaining_length;
-                    self.blocks[free_itt].last_mut().unwrap().length=remaining_length;
-                    self.blocks[free_itt].last_mut().unwrap().id = file_id;
-                    self.blocks[free_itt].push(Block { id: FREE, length: remain_free_len });
-                    self.blocks[block_itt][0].id = FREE;
+                    self.blocks[block_itt-1].free += remaining_length;
+                    self.blocks[block_itt].data.pop();
+                    self.blocks[free_itt].free = free_len - remaining_length;
+                    self.blocks[free_itt].data.push(Block { id: file_id, length: remaining_length });
                     break;
                 }
             }
             block_itt -= 1;
-            // println!("{:?}", self);
+            // println!("{:?} free_itt ({}) block_itt ({})", self, free_itt, block_itt);
         }
     }
 
     fn compress_without_frags(&mut self)  {
         let mut fsi = FreeSpanIndexor::new();
         for (idx, span) in self.blocks.iter().enumerate() {
-            if span.len() == 2 {
-                fsi.push(idx, span[1].length);
+            if span.free > 0 {
+                fsi.push(idx, span.free);
             }
         }
         let mut itt = self.blocks.len() - 1;
         while itt > 0 {
-            let cur_block = &mut self.blocks[itt][0];
-            if let Some(free_idx) = fsi.search_idx(cur_block.length, itt) {
-                let block_id = cur_block.id;
-                let block_len = cur_block.length;
-                cur_block.id = FREE;
-                let free_block = self.blocks[free_idx].last_mut().unwrap();
-                free_block.id = block_id;
-                if free_block.length < block_len {
-                    panic!("Error on search for id {} with len {} found id {} with len {}", itt, block_len, free_idx, free_block.length);
-                }
-                let free_len = free_block.length - block_len;
-                free_block.length = block_len;
-                if free_len > 0 {
-                    self.blocks[free_idx].push(Block { id: FREE, length: free_len });
-                }
+            let block_len = self.blocks[itt].data[0].length;
+            if let Some(free_idx) = fsi.search_idx(block_len, itt) {
+                self.blocks[itt-1].free += block_len;
+                let moved_data = self.blocks[itt].data.remove(0);
+                self.blocks[free_idx].data.push(moved_data);
+                self.blocks[free_idx].free -= block_len;
             }
             itt-=1;
         }
@@ -143,19 +150,18 @@ impl FileMap {
         let mut itt = 0;
         let mut checksum=0;
         for span in self.blocks.iter() {
-            for block in span {
-                if block.id != FREE {
-                    let to = block.length + itt - 1;
-                    let mut from = itt;
-                    if from > 0  {
-                        from -= 1;
-                    } 
-                    let sum = (to * (to + 1)) / 2 - ((from * (from + 1))/2);
-                    //println!("sum from {} to {} = {}", itt, to , sum);
-                    checksum += sum * block.id as usize;
-                }
+            for block in span.data.iter() {
+                let to = block.length + itt - 1;
+                let mut from = itt;
+                if from > 0  {
+                    from -= 1;
+                } 
+                let sum = (to * (to + 1)) / 2 - ((from * (from + 1))/2);
+                //println!("sum from {} to {} = {}", itt, to , sum);
+                checksum += sum * block.id as usize;
                 itt += block.length;
             }
+            itt += span.free;
         }
         checksum
     }
@@ -165,11 +171,13 @@ impl std::fmt::Debug for FileMap {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let mut file_map_state = Vec::new();
         for b in self.blocks.iter() {
-            for s in b.iter() {
+            for s in b.data.iter() {
                 for _ in 0..s.length {
-                    let e = if s.id == FREE { b'.' } else { b'0' + s.id as u8 };
-                    file_map_state .push(e);
+                    file_map_state .push(b'0' + s.id as u8);
                 }
+            }
+            for _ in 0..b.free {
+                file_map_state .push(b'.');
             }
         }
         write!(f, "{}", from_utf8(&file_map_state.as_slice()).unwrap())
