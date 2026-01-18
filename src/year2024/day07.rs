d1f98d302a9d8f9d718d646da767ccf4f4d6d376
Bernard Rodier <bernard.rodier@gmail.com>
2026-01-18 16:14:07 +0100

    replacing pointer method par match bloc

--- src/year2024/day07.rs
+++ src/year2024/day07.rs
@@ -3,42 +3,36 @@ use std::sync::atomic::Ordering::Relaxed;
 
 struct Equation {
     test_value:usize,
+    nb_eq:usize,
     numbers:Vec<usize>
 }
 
-const METHOD_2:[fn(usize,usize)->usize;3] = [Equation::multiply, Equation::add, Equation::join];
-const METHOD_1:[fn(usize,usize)->usize;2] = [Equation::add, Equation::multiply];
+#[inline]
+fn join(result:usize, value:usize) -> usize {
+    let mut pow = 10;
+    let mut v = value;
+    while v >= 10 {
+        v /= 10;
+        pow *= 10;
+    }
+    result * pow + value
+}
 
 impl Equation {
 
-    #[inline]
-    fn multiply(result:usize, value:usize) -> usize {
-        result * value
-    }
-
-    #[inline]
-    fn add(result:usize, value:usize) -> usize {
-        result + value
-    }
-
-    #[inline]
-    fn join(result:usize, value:usize) -> usize {
-        let mut pow = 10;
-        let mut v = value;
-        while v >= 10 {
-            v /= 10;
-            pow *= 10;
-        }
-        result * pow + value
-    }
-
-    fn iter_result(&self, idx:usize, tmp_result:usize, methods:&[fn(usize,usize)->usize]) -> bool {
+    fn iter_result(&self, idx:usize, tmp_result:usize) -> bool {
         if tmp_result > self.test_value {
             return false;
         }
         if idx < self.numbers.len() {
-            for method in methods {
-                if self.iter_result(idx+1, method(tmp_result, self.numbers[idx]), methods) {
+            for op in 0..self.nb_eq {
+                let next_result = match op {
+                    0 => tmp_result * self.numbers[idx],
+                    1 => tmp_result + self.numbers[idx],
+                    2 => join(tmp_result, self.numbers[idx]),
+                    _ => unreachable!()
+                };
+                if self.iter_result(idx+1, next_result) {
                     return true;
                 }
             }
@@ -48,15 +42,12 @@ impl Equation {
         }
     }
 
-    fn method(step:usize) -> &'static [fn(usize,usize)->usize] {
-        if step==1 { &METHOD_1 } else { &METHOD_2 }
-    }
 }
 
-fn parallel_solving(thread_id:usize, nb_threads:usize, step:usize, equations:&Vec<Equation>,total:&AtomicUsize) {
+fn parallel_solving(thread_id:usize, nb_threads:usize, equations:&Vec<Equation>,total:&AtomicUsize) {
     for i in (thread_id..equations.len()).step_by(nb_threads) {
         let e = &equations[i];
-        if e.iter_result(1, e.numbers[0], Equation::method(step)) {
+        if e.iter_result(1, e.numbers[0]) {
             total.fetch_add(e.test_value, Relaxed);
         }
     }
@@ -64,10 +55,11 @@ fn parallel_solving(thread_id:usize, nb_threads:usize, step:usize, equations:&Ve
 
 pub fn solve(step:usize, contents:String) -> String {
     let mut equations = Vec::new();
+    let nb_eq= if step == 1 { 2 } else { 3 };
     for line in contents.lines() {
         let (test_value, nums) = line.split_once(":").unwrap();
         let numbers:Vec<usize> = nums.trim().split(" ").map(|v| v.parse().unwrap()).collect();
-        equations.push(Equation{test_value:test_value.parse().unwrap(), numbers:numbers});
+        equations.push(Equation{test_value:test_value.parse().unwrap(), nb_eq, numbers:numbers});
     }
     let nb_threads = std::thread::available_parallelism().unwrap().get();
     let result = AtomicUsize::new(0);
@@ -75,7 +67,7 @@ pub fn solve(step:usize, contents:String) -> String {
     let list = &equations;
     scope(|scope| {
         for i in 0..nb_threads {
-            scope.spawn(move || parallel_solving(i, nb_threads, step, list, total));
+            scope.spawn(move || parallel_solving(i, nb_threads, list, total));
         }
     });
     result.into_inner().to_string()
