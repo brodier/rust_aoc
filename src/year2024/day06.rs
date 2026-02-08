449fb15ddbe9873a9ec5c877261883507c030bdb
Bernard Rodier <bernard.rodier@gmail.com>
2026-02-08 09:04:43 +0100

    Updating stucture to permit to remove fork (still to do)

--- src/year2024/day06.rs
+++ src/year2024/day06.rs
@@ -4,17 +4,19 @@ type State = ((usize, usize), Dir);
 
 struct Puzzle {
     state:State,
-    visited:[bool; 130*130*5],
+    nb_step:u16,
+    visited_state:[u16; 130*130*4],
+    visited_pos:[bool; 130*130],
     nb_visted:usize,
     nb_loop:usize,
 }
 
-fn idx(s:State) -> usize {
-    s.0.0 * 130 * 5 + s.0.1 * 5 + s.1 as usize
+fn st_idx(((x,y),d):State) -> usize {
+    y * 130 * 4 + x * 4 + d as usize
 }
 
-fn pos_idx(s:State) -> usize {
-    s.0.0 * 130 * 5 + s.0.1 * 5 + 4
+fn pos_idx(((x,y),_):State) -> usize {
+    y * 130 + x
 }
 
 impl Puzzle {
@@ -28,13 +30,16 @@ impl Puzzle {
         if  matches!(start.1, Dir::DOWN)  {
             unreachable!("start has not be initialized !");
         }
-        let mut visited = [false; 130*130*5];
-        visited[idx(start)] = true;
-         (Grid::build(input.to_string()), Puzzle{state: start, visited, nb_visted:0, nb_loop:0})
+        let mut visited_state = [0; 130*130*4];
+        visited_state[st_idx(start)] = 1;
+        let mut visited_pos = [false; 130*130];
+        visited_pos[pos_idx(start)] = true;
+
+         (Grid::build(input.to_string()), Puzzle{state: start, nb_step: 1, visited_state, visited_pos, nb_visted:1, nb_loop:0})
     }
 
     fn fork(&self) -> Puzzle {
-        Puzzle { state: self.state, visited: self.visited.clone(), nb_visted: self.nb_visted , nb_loop: self.nb_loop}
+        Puzzle { state: self.state, nb_step: self.nb_step, visited_state: self.visited_state.clone(), visited_pos: self.visited_pos, nb_visted: self.nb_visted , nb_loop: self.nb_loop}
     }
 
     fn walk(&mut self, grid:&mut Grid, part:usize) -> bool {
@@ -43,7 +48,7 @@ impl Puzzle {
             if grid.get(next_pos).unwrap() == b'#' {
                 self.state.1 = self.state.1.right()
             } else {
-                if !self.visited[pos_idx((next_pos,self.state.1))] {
+                if !self.visited_pos[pos_idx((next_pos,self.state.1))] {
                     // on walk 2 try to put block clone current puzzle and walk until end or loop
                     if part == 2 {
                         let previous = grid.set(next_pos, b'#').unwrap();
@@ -53,15 +58,16 @@ impl Puzzle {
                         }
                         let _ = grid.set(next_pos, previous);
                     }
-                    self.visited[pos_idx((next_pos,self.state.1))] = true;
+                    self.visited_pos[pos_idx((next_pos,self.state.1))] = true;
                     self.nb_visted += 1;
                 }
                 self.state.0 = next_pos;
             }
-            if self.visited[idx(self.state)] {
+            self.nb_step += 1;
+            if self.visited_state[st_idx(self.state)] > 0 && self.visited_state[st_idx(self.state)]  < self.nb_step {
                 return true;
             }
-            self.visited[idx(self.state)] = true;
+            self.visited_state[st_idx(self.state)] = self.nb_step;
         }
         return false;
     }
