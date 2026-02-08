d386cbdde83836df3fbdc431d1a28d93217d7876
Bernard Rodier <bernard.rodier@gmail.com>
2026-02-08 14:57:16 +0100

    improvement by avoiding fork

--- src/year2024/day06.rs
+++ src/year2024/day06.rs
@@ -2,12 +2,13 @@ use crate::utils::grid::*;
 
 type State = ((usize, usize), Dir);
 
-struct Puzzle {
+pub struct Puzzle {
     state:State,
-    nb_step:u16,
+    try_num:u16,
+    opt_block:bool,
     visited_state:[u16; 130*130*4],
     visited_pos:[bool; 130*130],
-    nb_visted:usize,
+    nb_visted:u16,
     nb_loop:usize,
 }
 
@@ -30,62 +31,58 @@ impl Puzzle {
         if  matches!(start.1, Dir::DOWN)  {
             unreachable!("start has not be initialized !");
         }
-        let mut visited_state = [0; 130*130*4];
-        visited_state[st_idx(start)] = 1;
+        let visited_state = [0; 130*130*4];
         let mut visited_pos = [false; 130*130];
         visited_pos[pos_idx(start)] = true;
-
-         (Grid::build(input.to_string()), Puzzle{state: start, nb_step: 1, visited_state, visited_pos, nb_visted:1, nb_loop:0})
+         (Grid::build(input.to_string()), Puzzle{state: start, try_num: 0, opt_block: true, visited_state, visited_pos, nb_visted:1, nb_loop:0})
     }
 
-    fn fork(&self) -> Puzzle {
-        Puzzle { state: self.state, nb_step: self.nb_step, visited_state: self.visited_state.clone(), visited_pos: self.visited_pos, nb_visted: self.nb_visted , nb_loop: self.nb_loop}
-    }
-
-    fn walk(&mut self, grid:&mut Grid, part:usize) -> bool {
+    fn walk(&mut self, grid:&mut Grid, _part:usize) -> bool {
         let gsize = grid.size();
         while let Ok(next_pos) = self.state.1.get_next(self.state.0,gsize) {
             if grid.get(next_pos).unwrap() == b'#' {
                 self.state.1 = self.state.1.right()
             } else {
                 if !self.visited_pos[pos_idx((next_pos,self.state.1))] {
-                    // on walk 2 try to put block clone current puzzle and walk until end or loop
-                    if part == 2 {
+                    if self.opt_block {
                         let previous = grid.set(next_pos, b'#').unwrap();
-                        let mut fork_puzzle = self.fork();
-                        if fork_puzzle.walk(grid,1) == true {
+                        self.opt_block = false;
+                        self.nb_visted += 1;
+                        self.try_num = self.nb_visted;
+                        let prev_state = self.state;
+                        if self.walk(grid,1) == true {
                             self.nb_loop+=1;
                         }
+                        self.try_num = 1;
+                        self.state = prev_state;
+                        self.opt_block = true;
                         let _ = grid.set(next_pos, previous);
+                        self.visited_pos[pos_idx((next_pos,self.state.1))] = true;
                     }
-                    self.visited_pos[pos_idx((next_pos,self.state.1))] = true;
-                    self.nb_visted += 1;
                 }
                 self.state.0 = next_pos;
             }
-            self.nb_step += 1;
-            if self.visited_state[st_idx(self.state)] > 0 && self.visited_state[st_idx(self.state)]  < self.nb_step {
+            if self.visited_state[st_idx(self.state)] == 1 || self.visited_state[st_idx(self.state)] == self.try_num {
                 return true;
             }
-            self.visited_state[st_idx(self.state)] = self.nb_step;
+            
+            self.visited_state[st_idx(self.state)] = self.try_num;
         }
         return false;
     }
 }
 
-pub fn parse(input:String) -> String {
-    input
+pub fn parse(input:String) -> Puzzle {
+    let (mut g, mut p) = Puzzle::build(&input);
+    p.walk(&mut g, 1);
+    p
 }
 
-pub fn part1(input:&str) -> String {
-    let (mut g, mut p) = Puzzle::build(input);
-    p.walk(&mut g, 1);
+pub fn part1(p:&Puzzle) -> String {
     p.nb_visted.to_string()
 }
 
 
-pub fn part2(input:&str) -> String {
-    let (mut g, mut p) = Puzzle::build(input);
-    p.walk(&mut g, 2);
+pub fn part2(p:&Puzzle) -> String {
     p.nb_loop.to_string()
 }
--- tests/year2024/day06.rs
+++ tests/year2024/day06.rs
@@ -15,10 +15,10 @@ const EXAMPLE:&str = "\
 
 #[test]
 fn part1_test() {
-    assert_eq!(part1(EXAMPLE), "41");
+    assert_eq!(part1(&parse(EXAMPLE.to_string())), "41");
 }
 
 #[test]
 fn part2_test() {
-    assert_eq!(part2(EXAMPLE), "6");
+    assert_eq!(part2(&parse(EXAMPLE.to_string())), "6");
 }
